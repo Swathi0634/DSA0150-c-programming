ea0d696f342916f77553c88944f7d210d72e7c76
Swathi0634 <152474883+Swathi0634@users.noreply.github.com>
2024-06-10 00:44:31 -0700

    Add files via upload

---
+++ celsius.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Function to convert Celsius to Fahrenheit
+float celsiusToFahrenheit(float celsius) {
+    return (9.0 / 5.0) * celsius + 32.0;
+}
+
+int main() {
+    float celsius;
+    cout << "Enter temperature in Celsius: ";
+    cin >> celsius;
+
+    float fahrenheit = celsiusToFahrenheit(celsius);
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    return 0;
+}
